40b0b6ce325a5e130464e30448ebbf5aecd7982a
Ubuntu <ubuntu@ip-172-31-19-203.ec2.internal>
2018-11-09 22:21:30 +0000

    experiments and pretrained models

--- lm-nidia27k-preprocess.py
+++ lm-nidia27k-preprocess.py
@@ -1,22 +1,25 @@
+
 from fastai import *        # Quick access to most common functionality
 from fastai.text import *   # Quick access to NLP functionality
-import fastai.datasets
+from pathlib import Path
 
-path_lm = '/home/ubuntu/data/medical/nidia27k_preprocess'
+path_lm = Path('/home/ubuntu/data/medical/nidia27k_preprocess')
 
 batch_size = 32
 epochs=20
-drop_mult=0.5
-
-data_lm = TextLMDataBunch.from_csv(path_lm)
-data_lm.train_dl.dl.bs = batch_size
-data_lm.valid_dl.dl.bs = batch_size
+drop_mult=0.75
 
 
-learn = RNNLearner.language_model(data_lm, drop_mult=drop_mult)
+data_lm = TextLMDataBunch.from_csv(path_lm,'documents-preprocess-valid.csv', classes=['neg','pos'], bs=batch_size)
+
+
+learn = language_model_learner(data_lm, drop_mult=0.3)
+
 learn.unfreeze()
-learn.fit(epochs, slice(1e-4,1e-3))
+learn.fit(epochs, slice(1e-4,1e-2))
 
 learn.save_encoder('enc_20_epochs_no_pretrain_nidia27k_preprocess')
+
+
 
 
--- lm-pretrained-nidia27k-preprocess.py
+++ lm-pretrained-nidia27k-preprocess.py
@@ -1,21 +1,21 @@
 from fastai import *        # Quick access to most common functionality
 from fastai.text import *   # Quick access to NLP functionality
-import fastai.datasets
+from pathlib import Path
 
-path_lm = '/home/ubuntu/data/medical/nidia27k_preprocess'
+path_lm = Path('/home/ubuntu/data/medical/nidia27k_preprocess')
 
-batch_size = 32
+batch_size = 16
 epochs=20
-drop_mult=0.5
-
-data_lm = TextLMDataBunch.from_csv(path_lm)
-data_lm.train_dl.dl.bs = batch_size
-data_lm.valid_dl.dl.bs = batch_size
+drop_mult=0.75
 
 
-learn = RNNLearner.language_model(data_lm, pretrained_model=URLs.WT103, drop_mult=drop_mult)
+data_lm = TextLMDataBunch.from_csv(path_lm,'documents-preprocess-valid.csv', classes=['neg','pos'])
+
+
+learn = language_model_learner(data_lm, drop_mult=0.3, pretrained_model=URLs.WT103)
+
 learn.unfreeze()
-learn.fit(epochs, slice(1e-4,1e-3))
+learn.fit(epochs, slice(1e-4,1e-2))
 
 learn.save_encoder('enc_20_epochs_nidia27k_preprocess')
 
