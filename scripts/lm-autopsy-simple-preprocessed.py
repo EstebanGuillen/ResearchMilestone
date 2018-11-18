e2b078b2bc3fc4be5d9ea197ae2075cdda58af2f
Ubuntu <ubuntu@ip-172-31-19-203.ec2.internal>
2018-11-18 07:21:15 +0000

    adding 5-fold crossvalidation and some new language model scripts

---
+++ scripts/lm-autopsy-simple-preprocessed.py
@@ -0,0 +1,23 @@
+
+from fastai import *        # Quick access to most common functionality
+from fastai.text import *   # Quick access to NLP functionality
+from pathlib import Path
+
+path_lm = Path('/home/ubuntu/data/autopsy')
+
+batch_size = 32
+epochs=12
+drop_mult=0.5
+
+
+data_lm = TextLMDataBunch.from_csv(path_lm,'data_suicide_homicide_k_1.csv', classes=['Suicide','Homicide'])
+
+
+learn = language_model_learner(data_lm, drop_mult=drop_mult, emb_sz=300, nh=198, nl=1)
+
+learn.unfreeze()
+learn.fit(epochs, slice(1e-4,1e-2))
+
+learn.save_encoder('enc_autopsy_not_pretrained_simple')
+
+
