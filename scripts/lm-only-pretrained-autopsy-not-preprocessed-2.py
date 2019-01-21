38a22f0999b4bb5b60af3a5ee0f16099c4613f8b
Ubuntu <ubuntu@ip-172-31-19-203.ec2.internal>
2019-01-21 06:43:40 +0000

    adding some notebooks that train on additional data

---
+++ scripts/lm-only-pretrained-autopsy-not-preprocessed-2.py
@@ -0,0 +1,14 @@
+from fastai import *        # Quick access to most common functionality
+from fastai.text import *   # Quick access to NLP functionality
+from pathlib import Path
+
+path_lm = Path('/home/ubuntu/data/autopsy/2014_2018/no_preprocessing')
+
+
+data_lm = TextLMDataBunch.from_csv(path_lm,'data_suicide_homicide_k_1.csv', classes=['Suicide','Homicide'])
+
+
+learn = language_model_learner(data_lm, pretrained_model=URLs.WT103)
+
+learn.save_encoder('enc_autopsy_only_pretrained_no_preprocessing')
+
